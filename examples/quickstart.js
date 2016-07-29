802429f5b5462f1bb2022f2a66ccf0d27e6f23ac
prabhatkm <prabhatkumar.mec@gmail.com>
2016-07-29 21:48:49 +0530

    sfsdj

--- examples/quickstart.js
+++ examples/quickstart.js
@@ -33,6 +33,7 @@ const firstEntityValue = (entities, entity) => {
 
 const actions = {
   send(request, response) {
+    console.log( request );
     const {sessionId, context, entities} = request;
     const {text, quickreplies} = response;
     return new Promise(function(resolve, reject) {
@@ -41,20 +42,95 @@ const actions = {
     });
   },
   getForecast({context, entities}) {
-    console.log("r",entities.location);
-    console.log("abc",entities[location]);
+    //console.log(entities);
+    console.log("forecast",context, entities);
     return new Promise(function(resolve, reject) {
       var location = firstEntityValue(entities, 'location')
+      console.log("location",location);
       if (location) {
         context.forecast = 'sunny in ' + location; // we should call a weather API here
-        delete context.missingLocation;
+        //delete context.missingLocation;
       } else {
-        context.missingLocation = true;
+        //context.missingLocation = true;
         delete context.forecast;
       }
       return resolve(context);
     });
+  } /* ,
+  getText({context}) {
+    console.log("tead");
+    return new Promise(function(resolve, reject) {
+        context.text = 'sunny'; // we should call a weather API here
+      return resolve(context);
+    });
   },
+   getText1({context,entities}) {
+    console.log("teadjdhj");
+
+    return new Promise(function(resolve, reject) {
+      var veg= null;
+      var intent = null;
+      var product = null;
+      var cuisine = null;
+      intent=firstEntityValue(entities, 'intent');
+      veg=firstEntityValue(entities, 'veg_nonveg');
+      product=firstEntityValue(entities, 'product');
+      cuisine=firstEntityValue(entities, 'cuisine');
+     
+      console.log(intent,cuisine,veg,product);
+      context.text = intent; // we should call a weather API here
+
+      if(intent === 'order' )
+      {
+     // console.log("entered if");
+        //if(product === 'food'){
+          if(veg != 'veg' && veg != 'nonveg' ){
+            context.text = "Veg or NonVeg ?";
+            //return reject(context); 
+          }
+          else if(cuisine === null ){
+            context.text = "Cuisine? "; 
+            //return reject(context);
+          }
+          else {
+              //delete context.entities;
+              context.text = "Your order will be placed for "+veg+" "+cuisine+" "+product;
+              
+          }
+          return resolve(context);
+        //}
+
+
+      } 
+       
+       
+       
+       /* 
+        if(intent=="order" && veg==null )
+        {
+          
+          context.text="veg or non-veg";
+          console.log(context.text);
+        }
+        if(veg!=null)
+        {
+          if(veg=="veg")
+          {
+            context.text="Restro1 , Restro2...";
+          }
+          else if(veg=="nonveg")
+          {
+            context.text="Restro 3, Restro 4";
+          }
+          else{
+            context.text="Ramdom restro";
+          }
+          //delete context.entities.veg_nonveg;
+        } /
+      //entities = null;
+      
+    });
+  } */
 };
 
 const client = new Wit({accessToken, actions});
